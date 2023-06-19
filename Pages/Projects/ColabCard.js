2d4fb1a098c32b8c8d821e59d118b15eb2d43734
RexPerG <rezaggholamali@gmail.com>
2023-06-19 10:19:22 +0100

    Added button styling

--- Pages/Projects/ColabCard.js
+++ Pages/Projects/ColabCard.js
@@ -1,8 +1,9 @@
-import { View, Text, Image, Linking, ScrollView } from "react-native";
-import React from "react";
+import { View, Text, Image, Linking, ScrollView } from 'react-native';
+import React from 'react';
+import { TouchableOpacity } from 'react-native-web';
 
 const ColabCard = ({ data }) => {
-    console.log(data)
+  console.log(data);
   return (
     <View className="flex flex-row border-[1px] border-black m-2 p-5 items-center">
       <Image
@@ -11,16 +12,19 @@ const ColabCard = ({ data }) => {
       />
 
       <View className="pl-3">
-        <Text className="font-bold">{data.item.username}</Text>
-        <Text className="font-semibold">{data.item.name}</Text>
-        <Text className="font-semibold">{data.item.description}</Text>
-
-        <Text
-          className="bg-black my-1 py-1 px-2 text-white text-center w-[80px]"
-          onPress={() =>  Linking.openURL(`${data.item.html_url}`)}
-        >
-          GitHub
+        <Text className="font-bold">User Name: {data.item.username}</Text>
+        <Text className="font-semibold">Project Name: {data.item.name}</Text>
+        <Text className="font-semibold">
+          Project Description: {data.item.description}
         </Text>
+        <TouchableOpacity>
+          <Text
+            className="bg-lime-700 my-1 py-1 px-2 text-white text-center w-[80px] rounded-full"
+            onPress={() => Linking.openURL(`${data.item.html_url}`)}
+          >
+            GitHub
+          </Text>
+        </TouchableOpacity>
       </View>
     </View>
   );
